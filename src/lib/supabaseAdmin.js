601c7c43a656b49d63a037adfdd18ba053fc599a
sxnisar2025 <sxnisar@gmail.com>
2026-07-14 17:23:55 +0500

    reset pass

--- src/lib/supabaseAdmin.js
+++ src/lib/supabaseAdmin.js
@@ -1,12 +1,22 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY,
-  {
-    auth: {
-      autoRefreshToken: false,
-      persistSession: false,
-    },
-  }
-);
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const key = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+console.log("URL:", url);
+console.log("KEY EXISTS:", !!key);
+
+if (!url) {
+  throw new Error("NEXT_PUBLIC_SUPABASE_URL is missing");
+}
+
+if (!key) {
+  throw new Error("SUPABASE_SERVICE_ROLE_KEY is missing");
+}
+
+export const supabaseAdmin = createClient(url, key, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+  },
+});
